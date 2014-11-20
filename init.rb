832621fd55469ccfac912740116d2a01107f6f09
Petr Pospíšil <petr@easy.cz>
2014-11-20 15:43:09 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :redmine_installer do
   name 'Redmine Installer plugin'
-  author 'EasyRedmine'
+  author 'Easy Redmine'
   description 'Easy way how to install/upgrade Redmine or plugin.'
   version '1.0.0'
   url 'https://github.com/easyredmine/installer'
